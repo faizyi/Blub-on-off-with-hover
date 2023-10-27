b9344ad983ca2dc584aa96d91c9f539ff454f850
faizyi <faiz557564@gmail.com>
2023-10-28 01:51:01 +0500

    work done

---
+++ Bulb on off with hover/app.js
@@ -0,0 +1,11 @@
+
+        
+        const img = document.querySelector('#img')
+        img.addEventListener('mouseenter', ()=>{
+            img.src='./img/bulb on.png'
+
+        })
+        img.addEventListener('mouseleave', ()=>{
+            img.src='./img/bulb off.png'
+
+        })
